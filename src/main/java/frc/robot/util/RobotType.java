b2a1284f9d0a43d2c0840c0cbc40a69dd1d5a423
Joseph Eng <91924258+KangarooKoala@users.noreply.github.com>
2025-01-08 17:23:18 -0800

    Move RobotType and MAC address detection (#1)

---
+++ src/main/java/frc/robot/util/RobotType.java
@@ -0,0 +1,36 @@
+package frc.robot.util;
+
+import edu.wpi.first.wpilibj.DriverStation;
+
+public enum RobotType {
+  COMPETITION(0x38, 0xd9, 0x93),
+  CRANE(0x22, 0xb0, 0x92),
+  BONK(0x33, 0x9d, 0xe7);
+
+  @SuppressWarnings("ImmutableEnumChecker")
+  private final MACAddress macAddress;
+
+  RobotType(int a, int b, int c) {
+    macAddress = MACAddress.of(a, b, c);
+  }
+
+  private static RobotType detectType() {
+    for (RobotType robotType : RobotType.values()) {
+      if (robotType.macAddress.exists()) {
+        return robotType;
+      }
+    }
+    DriverStation.reportWarning(
+        "Code running on unknown MAC Address! Running competition code anyways", false);
+    return COMPETITION;
+  }
+
+  private static RobotType currentRobotType;
+
+  public static RobotType getCurrent() {
+    if (currentRobotType == null) {
+      currentRobotType = detectType();
+    }
+    return currentRobotType;
+  }
+}
