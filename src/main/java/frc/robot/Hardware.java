5608ea5852347fa2852750857c09874f2bede630
Polyehteretherketone <s-parrisr@bsd405.org>
2025-01-18 10:40:07 -0800

    Update Hardware.java Arm pivot can ids (#4)
    * Update Hardware.java
    
    * Update Hardware.java

--- src/main/java/frc/robot/Hardware.java
+++ src/main/java/frc/robot/Hardware.java
@@ -8,4 +8,7 @@ public class Hardware {
   // elevator [20-29]
   public static final int ELEVATOR_MOTOR_ONE = 20;
   public static final int ELEVATOR_MOTOR_TWO = 21;
+
+  // arm pivot [30-39]
+  public static final int ARM_PIVOT_MOTOR_ID = 30;
 }
