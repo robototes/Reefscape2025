8f0b8c43cdb545a00421a114f45f0c21b8b27883
braelynandthefrogs <siennafox@gmail.com>
2025-01-18 10:11:16 -0800

    elevator motor IDs created. currently occupying spaces 21 and 22

--- src/main/java/frc/robot/Hardware.java
+++ src/main/java/frc/robot/Hardware.java
@@ -2,4 +2,8 @@ package frc.robot;
 
 public class Hardware {
   // Add motor IDs here
+
+  //elevator [20-29]
+  public static final int ELEVATOR_MOTOR_ONE = 20;
+  public static final int ELEVATOR_MOTOR_TWO = 21;
 }
