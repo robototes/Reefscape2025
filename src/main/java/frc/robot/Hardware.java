53e3a06ca90a95c436e3a0c4beed06208c5401c8
iamawesomecat <145405364+iamawesomecat@users.noreply.github.com>
2025-01-18 10:30:42 -0800

    Singular comment for swerve ID's (#5)
    * added swerve comment
    
    In hardware file, added comment
    
    //Swerve: 1-12
    
    * Spotless 💀
    
    bruh
    
    Merged with braelyns ID's

--- src/main/java/frc/robot/Hardware.java
+++ src/main/java/frc/robot/Hardware.java
@@ -3,6 +3,8 @@ package frc.robot;
 public class Hardware {
   // Add motor IDs here
 
+  // Swerve: 1-12
+
   // elevator [20-29]
   public static final int ELEVATOR_MOTOR_ONE = 20;
   public static final int ELEVATOR_MOTOR_TWO = 21;
