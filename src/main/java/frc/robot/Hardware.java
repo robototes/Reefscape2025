c0e9739efecaf633cbcc5612392e84c82cc5c229
braelynandthefrogs <siennafox@gmail.com>
2025-01-18 10:23:48 -0800

    ran spotless on my elevator motor ID hardware

--- src/main/java/frc/robot/Hardware.java
+++ src/main/java/frc/robot/Hardware.java
@@ -3,7 +3,7 @@ package frc.robot;
 public class Hardware {
   // Add motor IDs here
 
-  //elevator [20-29]
+  // elevator [20-29]
   public static final int ELEVATOR_MOTOR_ONE = 20;
   public static final int ELEVATOR_MOTOR_TWO = 21;
 }
