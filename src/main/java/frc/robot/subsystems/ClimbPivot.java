c6811e4c98e2f5f6bfca210dffff8d70e4aebd74
iamawesomecat <iamawesomecat2@gmail.com>
2025-01-23 18:39:23 -0800

    First command, configureMotor method

--- src/main/java/frc/robot/subsystems/ClimbPivot.java
+++ src/main/java/frc/robot/subsystems/ClimbPivot.java
@@ -1,6 +1,10 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
+import com.ctre.phoenix6.configs.SoftwareLimitSwitchConfigs;
 import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Hardware;
 
@@ -12,5 +16,26 @@ public class ClimbPivot extends SubsystemBase {
   public ClimbPivot() {
     climbPivotMotorOne = new TalonFX(Hardware.CLIMB_PIVOT_MOTOR_ONE_ID);
     climbPivotMotorTwo = new TalonFX(Hardware.CLIMB_PIVOT_MOTOR_TWO_ID);
+
+    configureMotor();
   }
+
+  public void configureMotor(){
+    var talonFXConfigurator = climbPivotMotorOne.getConfigurator();
+    var talonFXConfigurator2 = climbPivotMotorTwo.getConfigurator();
+
+    var currentLimits = new CurrentLimitsConfigs();
+
+    // enable stator current limit
+    currentLimits.StatorCurrentLimit = 5; 
+    currentLimits.StatorCurrentLimitEnable = true;
+    currentLimits.SupplyCurrentLimit = 5; 
+    currentLimits.SupplyCurrentLimitEnable = true;
+    talonFXConfigurator.apply(currentLimits);
+    talonFXConfigurator2.apply(currentLimits);
+  }
+  
+    public Command MoveClimbMotor(double speed){
+            return runOnce(() -> {climbPivotMotorOne.set(speed); climbPivotMotorTwo.set(-speed);});
+    }   
 }
