1e4e53a7a6d3556b814f016be45d0051b7ea00fc
sabellad <s-donofrios@bsd405.org>
2025-01-27 19:12:30 -0800

    added branch sensors (#15)
    * added branch sensors
    
    * created a generic get distance method for sensors, called it in get left and right sensor distance, should be ready to merge to main
    
    * added branch sensors to Sensors file
    
    * made a method only called in branchsensors private
    
    ---------
    
    Co-authored-by: braelynandthefrogs <97708418+braelynandthefrogs@users.noreply.github.com>
    Co-authored-by: braelynandthefrogs <siennafox@gmail.com>

--- src/main/java/frc/robot/Hardware.java
+++ src/main/java/frc/robot/Hardware.java
@@ -5,9 +5,13 @@ public class Hardware {
 
   // Swerve: 1-12
 
-  // elevator [20-29]
+  // elevator [20-24]
   public static final int ELEVATOR_MOTOR_ONE = 20;
   public static final int ELEVATOR_MOTOR_TWO = 21;
+
+  // branch sensors [25-29] (mounted on elevator)
+  public static final int BRANCH_SENSOR_LEFT = 25;
+  public static final int BRANCH_SENSOR_RIGHT = 26;
 
   // arm pivot [30-34]
   public static final int ARM_PIVOT_MOTOR_ID = 30;
