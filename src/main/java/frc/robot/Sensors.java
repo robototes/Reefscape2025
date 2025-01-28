84f80f775dac1b5cfd5f873a957ebc7adf252431
braelynandthefrogs <siennafox@gmail.com>
2025-01-27 19:01:41 -0800

    added branch sensors to Sensors file

--- src/main/java/frc/robot/Sensors.java
+++ src/main/java/frc/robot/Sensors.java
@@ -3,15 +3,18 @@ package frc.robot;
 import static frc.robot.Sensors.SensorConstants.*;
 
 import frc.robot.sensors.ArmSensor;
+import frc.robot.sensors.BranchSensors;
 
 public class Sensors {
   public static class SensorConstants {
     // <SENSOR>_ENABLED constants go here
     public static final boolean ARMSENSOR_ENABLED = true;
+    public static final boolean BRANCHSENSORS_ENABLED = true;
   }
 
   // Sensors go here
   public final ArmSensor armSensor;
+  public final BranchSensors branchSensors;
 
   public Sensors() {
     // Initialize subsystems here (don't forget to check if they're enabled!)
@@ -20,6 +23,11 @@ public class Sensors {
       armSensor = new ArmSensor();
     } else {
       armSensor = null;
+    }
+    if (BRANCHSENSORS_ENABLED) {
+      branchSensors = new BranchSensors();
+    } else {
+      branchSensors = null;
     }
   }
 }
