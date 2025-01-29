20d20f3938997178c20632f7ef34589183049c73
Polyehteretherketone <s-parrisr@bsd405.org>
2025-01-28 18:23:01 -0800

    created new subsystem
    Added

--- src/main/java/frc/robot/sensors/ArmSensor.java
+++ src/main/java/frc/robot/sensors/ArmSensor.java
@@ -9,29 +9,29 @@ import frc.robot.Hardware;
 
 public class ArmSensor {
 
-    private final LaserCan mainSensor;
+  private final LaserCan mainSensor;
 
-    public ArmSensor() {
-        mainSensor = new LaserCan(Hardware.MAIN_ARM_SENSOR);
-        ConfigureSensor(mainSensor);
-    }
-    
-    private void ConfigureSensor(LaserCan Sensor) {
-        try {
-          Sensor.setRangingMode(LaserCan.RangingMode.SHORT);
-          Sensor.setRegionOfInterest(new LaserCan.RegionOfInterest(8, 8, 16, 16));
-          Sensor.setTimingBudget(LaserCan.TimingBudget.TIMING_BUDGET_33MS);
-        } catch (ConfigurationFailedException e) {
-          System.out.println("Configuration Failed! " + e);
-        }
-    }
+  public ArmSensor() {
+    mainSensor = new LaserCan(Hardware.MAIN_ARM_SENSOR);
+    ConfigureSensor(mainSensor);
+  }
 
-    public Distance getSensorDistance() {
-        LaserCan.Measurement measurement = mainSensor.getMeasurement();
-        if (measurement != null && measurement.status == LaserCan.LASERCAN_STATUS_VALID_MEASUREMENT) {
-          return Millimeter.of(measurement.distance_mm);
-        } else {
-          return Millimeter.of(-1);
-        }
+  private void ConfigureSensor(LaserCan Sensor) {
+    try {
+      Sensor.setRangingMode(LaserCan.RangingMode.SHORT);
+      Sensor.setRegionOfInterest(new LaserCan.RegionOfInterest(8, 8, 16, 16));
+      Sensor.setTimingBudget(LaserCan.TimingBudget.TIMING_BUDGET_33MS);
+    } catch (ConfigurationFailedException e) {
+      System.out.println("Configuration Failed! " + e);
     }
+  }
+
+  public Distance getSensorDistance() {
+    LaserCan.Measurement measurement = mainSensor.getMeasurement();
+    if (measurement != null && measurement.status == LaserCan.LASERCAN_STATUS_VALID_MEASUREMENT) {
+      return Millimeter.of(measurement.distance_mm);
+    } else {
+      return Millimeter.of(-1);
+    }
+  }
 }
---
+++ src/main/java/frc/robot/subsystems/SpinnyClaw.java
@@ -0,0 +1,54 @@
+package frc.robot.subsystems;
+
+import static edu.wpi.first.units.Units.Volts;
+
+import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.configs.TalonFXConfigurator;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+import edu.wpi.first.units.measure.Voltage;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Hardware;
+import java.util.function.Supplier;
+
+public class SpinnyClaw extends SubsystemBase {
+  // Remove once we implement PID speed
+  public static int placeholderPIDSpeed;
+
+  // TalonFX
+  private final TalonFX motor;
+
+  public SpinnyClaw() {
+    motor = new TalonFX(Hardware.SPINNY_LAW_MOTOR_ID);
+    factoryDefaults();
+  }
+
+  // (+) is to move arm up, and (-) is down
+  public Command startMovingVoltage(Supplier<Voltage> speedControl) {
+    return run(() -> motor.setVoltage(speedControl.get().in(Volts)));
+  }
+
+  public void moveArmCoral(int preset) {}
+
+  public void moveArmAlgae(int preset) {}
+
+  // TalonFX config
+  public void factoryDefaults() {
+    TalonFXConfiguration configuration = new TalonFXConfiguration();
+    TalonFXConfigurator cfg = motor.getConfigurator();
+    var currentLimits = new CurrentLimitsConfigs();
+    configuration.MotorOutput.NeutralMode = NeutralModeValue.Brake;
+    configuration.ClosedLoopGeneral.ContinuousWrap = true;
+    cfg.apply(configuration);
+    // enabling stator current limits
+    currentLimits.StatorCurrentLimit = 5; // starting low for testing
+    currentLimits.StatorCurrentLimitEnable = true;
+    currentLimits.SupplyCurrentLimit = 5; // starting low for testing
+    currentLimits.SupplyCurrentLimitEnable = true;
+    cfg.apply(currentLimits);
+  }
+
+
+}
