84e2940f05610ba35845d7a9cafd964745c49d77
iamawesomecat <iamawesomecat2@gmail.com>
2025-01-30 18:53:58 -0800

    climb fixes
    Did requested climb fixes.
    
    learning how to break mode on neutral was "fun"

--- src/main/java/frc/robot/subsystems/ClimbPivot.java
+++ src/main/java/frc/robot/subsystems/ClimbPivot.java
@@ -1,7 +1,9 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
+import com.ctre.phoenix6.configs.MotorOutputConfigs;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.NeutralModeValue;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Hardware;
@@ -15,12 +17,13 @@ public class ClimbPivot extends SubsystemBase {
     climbPivotMotorOne = new TalonFX(Hardware.CLIMB_PIVOT_MOTOR_ONE_ID);
     climbPivotMotorTwo = new TalonFX(Hardware.CLIMB_PIVOT_MOTOR_TWO_ID);
 
-    configureMotor();
+    configureMotors();
   }
 
   private void configureMotors() {
     var talonFXConfigurator = climbPivotMotorOne.getConfigurator();
     var talonFXConfigurator2 = climbPivotMotorTwo.getConfigurator();
+    var talonFXMotorOutput = new MotorOutputConfigs();
 
     var currentLimits = new CurrentLimitsConfigs();
 
@@ -29,15 +32,22 @@ public class ClimbPivot extends SubsystemBase {
     currentLimits.StatorCurrentLimitEnable = true;
     currentLimits.SupplyCurrentLimit = 5;
     currentLimits.SupplyCurrentLimitEnable = true;
+    talonFXMotorOutput.withNeutralMode(NeutralModeValue.Brake);
     talonFXConfigurator.apply(currentLimits);
     talonFXConfigurator2.apply(currentLimits);
+    talonFXConfigurator.apply(talonFXMotorOutput);
+    talonFXConfigurator2.apply(talonFXMotorOutput);
   }
 
   public Command moveClimbMotor(double speed) {
-    return runOnce(
-        () -> {
+    return run(() -> {
           climbPivotMotorOne.set(speed);
           climbPivotMotorTwo.set(-speed);
-        });
+        })
+        .finallyDo(
+            () -> {
+              climbPivotMotorOne.stopMotor();
+              climbPivotMotorTwo.stopMotor();
+            });
   }
 }
