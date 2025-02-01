0c1210ceb80a3c44c20ab05fa1e86e22e1f96f8a
Joseph Eng <91924258+KangarooKoala@users.noreply.github.com>
2025-02-01 14:04:27 -0800

    Add comp bot swerve (#17)
    Implemented swerve for the 2025 comp chassis 
    Generated constants for the 2025 comp bot
    Fixed Mac Address issues
    ---------
    
    Co-authored-by: RobototesProgrammers <194180343+RobototesProgrammers@users.noreply.github.com>
    Co-authored-by: iamawesomecat <145405364+iamawesomecat@users.noreply.github.com>

--- src/main/java/frc/robot/Subsystems.java
+++ src/main/java/frc/robot/Subsystems.java
@@ -3,12 +3,14 @@ package frc.robot;
 import static frc.robot.Subsystems.SubsystemConstants.*;
 
 import frc.robot.generated.BonkTunerConstants;
+import frc.robot.generated.CompTunerConstants;
 import frc.robot.subsystems.ArmPivot;
 import frc.robot.subsystems.DrivebaseWrapper;
 import frc.robot.subsystems.ElevatorSubsystem;
 import frc.robot.subsystems.SpinnyClaw;
 import frc.robot.subsystems.VisionSubsystem;
 import frc.robot.subsystems.drivebase.CommandSwerveDrivetrain;
+import frc.robot.util.RobotType;
 
 public class Subsystems {
   public static class SubsystemConstants {
@@ -35,7 +37,11 @@ public class Subsystems {
     // Initialize subsystems here (don't forget to check if they're enabled!)
     // Add specification for bonk, Enum? get team number?
     if (DRIVEBASE_ENABLED) {
-      drivebaseSubsystem = BonkTunerConstants.createDrivetrain();
+      if (RobotType.getCurrent() == RobotType.BONK) {
+        drivebaseSubsystem = BonkTunerConstants.createDrivetrain();
+      } else {
+        drivebaseSubsystem = CompTunerConstants.createDrivetrain();
+      }
       drivebaseWrapper = new DrivebaseWrapper(drivebaseSubsystem);
     } else {
       drivebaseSubsystem = null;
