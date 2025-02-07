b6334907c454d4a92ae4801b80ee8bfe0fc321d4
sabellad <s-donofrios@bsd405.org>
2025-02-06 20:04:33 -0800

    added more modes for the lights

--- src/main/java/frc/robot/sensors/ElevatorLight.java
+++ src/main/java/frc/robot/sensors/ElevatorLight.java
@@ -4,8 +4,13 @@ import com.ctre.phoenix.led.Animation;
 import com.ctre.phoenix.led.CANdle;
 import com.ctre.phoenix.led.CANdle.LEDStripType;
 import com.ctre.phoenix.led.CANdleConfiguration;
+import com.ctre.phoenix.led.ColorFlowAnimation;
+import com.ctre.phoenix.led.FireAnimation;
 import com.ctre.phoenix.led.LarsonAnimation;
 import com.ctre.phoenix.led.RainbowAnimation;
+import com.ctre.phoenix.led.RgbFadeAnimation;
+import com.ctre.phoenix.led.SingleFadeAnimation;
+import com.ctre.phoenix.led.StrobeAnimation;
 import com.ctre.phoenix.led.TwinkleAnimation;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -18,15 +23,27 @@ public class ElevatorLight extends SubsystemBase {
   // LED modes
   public RainbowAnimation rainbowAnim = new RainbowAnimation(.5, .89, 64);
   public LarsonAnimation larsonAnim = new LarsonAnimation(177, 156, 217);
-  public TwinkleAnimation twinkleAnim = new TwinkleAnimation(204, 100, 255);
+  public TwinkleAnimation twinkleAnim = new TwinkleAnimation(135, 30, 270);
+  public ColorFlowAnimation colorFlowAnim = new ColorFlowAnimation(4, 14, 30);
+  public FireAnimation fireAnim = new FireAnimation(); // dont just dont
+  public RgbFadeAnimation rgbFadeAnim = new RgbFadeAnimation(); // can we change it?
+  public SingleFadeAnimation singleFadeAnim = new SingleFadeAnimation(50, 30, 270);
+  public StrobeAnimation strobeAnim = new StrobeAnimation(24, 15, 204);
 
   public ElevatorLight() {
+
     candle = new CANdle(Hardware.ELEVATOR_LED);
     configureCandle();
-    // candle.setLEDs(28, 3, 3);
+    candle.setLEDs(28, 3, 3);
     // candle.animate(larsonAnim);
     // candle.animate(rainbowAnim);
-    candle.animate(twinkleAnim);
+    // candle.animate(twinkleAnim);
+    // candle.animate(colorFlowAnim);
+    // candle.animate(fireAnim);
+    // candle.animate(rgbFadeAnim);
+    // candle.animate(singleFadeAnim);
+    // candle.animate(strobeAnim);
+
   }
 
   private void configureCandle() {
