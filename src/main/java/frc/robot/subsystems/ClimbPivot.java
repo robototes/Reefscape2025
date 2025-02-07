3ff4b5a820c1bb8a8d104643870edab7f1105d5e
iamawesomecat <iamawesomecat2@gmail.com>
2025-02-06 18:44:46 -0800

    Added trigger control
    Co-Authored-By: Joseph Eng <91924258+KangarooKoala@users.noreply.github.com>

--- src/main/java/frc/robot/subsystems/ClimbPivot.java
+++ src/main/java/frc/robot/subsystems/ClimbPivot.java
@@ -7,6 +7,7 @@ import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Hardware;
 
@@ -15,6 +16,14 @@ public class ClimbPivot extends SubsystemBase {
   private final TalonFX climbPivotMotorOne;
   private final TalonFX climbPivotMotorTwo;
   private final DigitalInput climbSensor;
+
+  private final double CLIMB_OUT_PRESET = 90;
+  private final double CLIMB_IN_PRESET = 0;
+  private final double CLIMB_IN_SPEED = 0.5;
+  private final double CLIMB_OUT_SPEED = -0.5;
+  private boolean isClimbOut = false;
+  private boolean isClimbIn = true;
+  private boolean nextMoveOut = true;
 
   public ClimbPivot() {
     climbPivotMotorOne = new TalonFX(Hardware.CLIMB_PIVOT_MOTOR_ONE_ID);
@@ -55,7 +64,46 @@ public class ClimbPivot extends SubsystemBase {
             });
   }
 
+  public Command toggleClimb() {
+    return Commands.either(
+        startEnd(
+                () -> {
+                  // climb out
+                  nextMoveOut = false;
+                  climbPivotMotorOne.set(CLIMB_OUT_SPEED);
+                },
+                () -> {
+                  climbPivotMotorOne.stopMotor();
+                })
+            .until(() -> isClimbOut),
+        startEnd(
+                () -> {
+                  // climb in
+                  nextMoveOut = true;
+                  climbPivotMotorOne.set(CLIMB_IN_SPEED);
+                },
+                () -> {
+                  climbPivotMotorOne.stopMotor();
+                })
+            .until(() -> !isClimbIn),
+        () -> nextMoveOut);
+  }
+
   public boolean checkClimbSensor() {
     return climbSensor.get();
+  }
+
+  @Override
+  public void periodic() {
+    if (climbPivotMotorOne.getPosition().getValueAsDouble() == CLIMB_OUT_PRESET) {
+      isClimbOut = true;
+    } else {
+      isClimbOut = false;
+    }
+    if (climbPivotMotorOne.getPosition().getValueAsDouble() == CLIMB_IN_PRESET) {
+      isClimbIn = true;
+    } else {
+      isClimbIn = false;
+    }
   }
 }
