b3853c7a073dff58e10791359e49ff66c2119098
RobototesProgrammers <194180343+RobototesProgrammers@users.noreply.github.com>
2025-02-18 17:20:40 -0800

    Enable arm pivot

--- src/main/java/frc/robot/Subsystems.java
+++ src/main/java/frc/robot/Subsystems.java
@@ -21,7 +21,7 @@ public class Subsystems {
     public static final boolean DRIVEBASE_ENABLED = false;
     public static final boolean VISION_ENABLED = false;
     public static final boolean ELEVATOR_ENABLED = false;
-    public static final boolean ARMPIVOT_ENABLED = false;
+    public static final boolean ARMPIVOT_ENABLED = true;
     public static final boolean SPINNYCLAW_ENABLED = false;
     public static final boolean CLIMBPIVOT_ENABLED = false;
     public static final boolean ELEVATOR_LED_ENABLED = false;
