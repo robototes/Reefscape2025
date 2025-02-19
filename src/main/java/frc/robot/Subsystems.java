83756141ea39b3251c2882529a658a1cf4bf6ac3
Joseph Eng <91924258+KangarooKoala@users.noreply.github.com>
2025-02-18 17:15:42 -0800

    2/16-2/18 elevator testing (#33)
    * 2/16 testing changes
    
    re-add follower control to motor 2?
    
    Motors likely wouldnt follow eachother bcs of one motor having soft limits and other not.
    
    TODO: Re-add follower mode?
    
    * WIP work from 2/17
    Need to figure out following mode
    
    * 2/17 testing and 2/18 logging
    
    * applying config to 2nd motor and fixing log name
    
    * naming commands
    
    * spotless
    
    * Remove redundant logging of CommandScheduler
    
    * Add rumble when not zero'd, move setTargetPosition, try stopping motors at end of power manual control
    
    * Initial elevator preset heights
    
    ---------
    
    Co-authored-by: RobototesProgrammers <194180343+RobototesProgrammers@users.noreply.github.com>

--- src/main/java/frc/robot/Subsystems.java
+++ src/main/java/frc/robot/Subsystems.java
@@ -18,9 +18,9 @@ public class Subsystems {
   public static class SubsystemConstants {
     // <SUBSYSTEM>_ENABLED constants go here
 
-    public static final boolean DRIVEBASE_ENABLED = false;
+    public static final boolean DRIVEBASE_ENABLED = true;
     public static final boolean VISION_ENABLED = false;
-    public static final boolean ELEVATOR_ENABLED = false;
+    public static final boolean ELEVATOR_ENABLED = true;
     public static final boolean ARMPIVOT_ENABLED = false;
     public static final boolean SPINNYCLAW_ENABLED = false;
     public static final boolean CLIMBPIVOT_ENABLED = false;
