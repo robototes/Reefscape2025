fcdb8742ce38f0f4fcdcbfba1ccdee1384b1acb0
DriverStation3 <194180343+RobototesProgrammers@users.noreply.github.com>
2025-02-20 12:28:41 -0800

    Accurate poses for red side branches
    Accurate poses for red side branches

--- src/main/java/frc/robot/subsystems/auto/AutoAlign.java
+++ src/main/java/frc/robot/subsystems/auto/AutoAlign.java
@@ -47,32 +47,32 @@ public class AutoAlign {
 	private static final List<Pose2d> blueBranchesPoses = Arrays.asList(BRANCH_B_A, BRANCH_B_B, BRANCH_B_C, BRANCH_B_D, BRANCH_B_E, BRANCH_B_F, BRANCH_B_G, BRANCH_B_H, BRANCH_B_I, BRANCH_B_J, BRANCH_B_K, BRANCH_B_L);
 
 	private static final Pose2d BRANCH_R_A =
-			new Pose2d(new Translation2d(3.117, 4.181), Rotation2d.fromDegrees(0));
+			new Pose2d(new Translation2d(14.45, 3.855), Rotation2d.fromDegrees(180));
 	private static final Pose2d BRANCH_R_B =
-			new Pose2d(new Translation2d(3.117, 3.855), Rotation2d.fromDegrees(0));
+			new Pose2d(new Translation2d(14.45, 4.181), Rotation2d.fromDegrees(180));
 	private static final Pose2d BRANCH_R_C =
-			new Pose2d(new Translation2d(3.668, 2.910), Rotation2d.fromDegrees(60));
+			new Pose2d(new Translation2d(13.882, 5.200), Rotation2d.fromDegrees(240));
 	private static final Pose2d BRANCH_R_D =
-			new Pose2d(new Translation2d(3.920, 2.706), Rotation2d.fromDegrees(60));
+			new Pose2d(new Translation2d(13.618, 5.308), Rotation2d.fromDegrees(240));
 	private static final Pose2d BRANCH_R_E =
-			new Pose2d(new Translation2d(5.035, 2.706), Rotation2d.fromDegrees(120));	
+			new Pose2d(new Translation2d(12.491, 5.308), Rotation2d.fromDegrees(300));	
 	private static final Pose2d BRANCH_R_F =
-			new Pose2d(new Translation2d(5.311, 2.910), Rotation2d.fromDegrees(120));	
+			new Pose2d(new Translation2d(12.203, 5.200), Rotation2d.fromDegrees(300));	
 	private static final Pose2d BRANCH_R_G =
-			new Pose2d(new Translation2d(5.874, 3.855), Rotation2d.fromDegrees(180));
+			new Pose2d(new Translation2d(11.628, 4.181), Rotation2d.fromDegrees(0));
 	private static final Pose2d BRANCH_R_H =
-			new Pose2d(new Translation2d(5.874, 4.181), Rotation2d.fromDegrees(180));	
+			new Pose2d(new Translation2d(11.628, 3.855), Rotation2d.fromDegrees(0));	
 	private static final Pose2d BRANCH_R_I =
-			new Pose2d(new Translation2d(5.311, 5.092), Rotation2d.fromDegrees(240));
+			new Pose2d(new Translation2d(12.203, 2.910), Rotation2d.fromDegrees(60));
 	private static final Pose2d BRANCH_R_J =
-			new Pose2d(new Translation2d(5.035, 5.308), Rotation2d.fromDegrees(240));
+			new Pose2d(new Translation2d(12.491, 2.682), Rotation2d.fromDegrees(60));
 	private static final Pose2d BRANCH_R_K =
-			new Pose2d(new Translation2d(5.035, 5.308), Rotation2d.fromDegrees(300));
+			new Pose2d(new Translation2d(13.618, 2.682), Rotation2d.fromDegrees(120));
 	private static final Pose2d BRANCH_R_L =
-			new Pose2d(new Translation2d(3.668, 5.092), Rotation2d.fromDegrees(300));
+			new Pose2d(new Translation2d(13.882, 2.910), Rotation2d.fromDegrees(120));
 	private static final List<Pose2d> redBranchesPoses = Arrays.asList(BRANCH_R_A, BRANCH_R_B, BRANCH_R_C, BRANCH_R_D, BRANCH_R_E, BRANCH_R_F, BRANCH_R_G, BRANCH_R_H, BRANCH_R_I, BRANCH_R_J, BRANCH_R_K, BRANCH_R_L);
 
-	public static Command autoAlign(
+	private static Command autoPathAlign(
 		CommandSwerveDrivetrain drivebaseSubsystem) {
 
 		Pose2d robotPose = drivebaseSubsystem.getState().Pose;
@@ -106,41 +106,7 @@ public class AutoAlign {
 		return AutoBuilder.followPath(path);
 	}
 
-	public static class AlignCommand extends Command {
-		private final CommandSwerveDrivetrain drivebaseSubsystem;
-		private Command alignCommand = null;
-
-		public AlignCommand(
-			CommandSwerveDrivetrain drivebaseSubsystem) {
-			this.drivebaseSubsystem = drivebaseSubsystem;
-			addRequirements(drivebaseSubsystem);
-		}
-
-		@Override
-		public void initialize() {
-			alignCommand = autoAlign(drivebaseSubsystem);
-
-			alignCommand.initialize();
-			// launcherSubsystem.setAngle(LauncherSubsystem.TRAP_AIM_ANGLE);
-			// launcherSubsystem.launch(LauncherSubsystem.TRAP_SHOOT_SPEED_RPM);
-		}
-
-		@Override
-		public void execute() {
-			alignCommand.execute();
-		}
-
-		@Override
-		public boolean isFinished() {
-			return alignCommand.isFinished();
-		}
-
-		@Override
-		public void end(boolean interrupted) {
-			if (interrupted && alignCommand != null) {
-				alignCommand.end(true);
-			}
-			alignCommand = null;
-		}
+	public static Command autoAlign(CommandSwerveDrivetrain drivebaseSubsystem ) {
+		return drivebaseSubsystem.defer(() -> autoPathAlign(drivebaseSubsystem));
 	}
 }
