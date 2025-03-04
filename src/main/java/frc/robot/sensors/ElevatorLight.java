129bef911247e019fd2847faff6c7e471314243e
sabellad <s-donofrios@bsd405.org>
2025-03-03 18:07:34 -0800

    spotless

--- src/main/java/frc/robot/sensors/ElevatorLight.java
+++ src/main/java/frc/robot/sensors/ElevatorLight.java
@@ -12,7 +12,6 @@ import com.ctre.phoenix.led.RgbFadeAnimation;
 import com.ctre.phoenix.led.SingleFadeAnimation;
 import com.ctre.phoenix.led.StrobeAnimation;
 import com.ctre.phoenix.led.TwinkleAnimation;
-
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj.AddressableLEDBufferView;
 import edu.wpi.first.wpilibj.LEDPattern;
@@ -26,17 +25,19 @@ public class ElevatorLight extends SubsystemBase {
 
   // private String curAnimation = "default";
   private final AddressableLEDBuffer buffer = new AddressableLEDBuffer(300);
-  private final AddressableLEDBufferView rightSection = buffer.createView(0, 100); 
+  private final AddressableLEDBufferView rightSection = buffer.createView(0, 100);
   private final AddressableLEDBufferView topRightSection = buffer.createView(100, 150).reversed();
-  private final AddressableLEDBufferView topLeftSection = buffer.createView(150, 200); 
+  private final AddressableLEDBufferView topLeftSection = buffer.createView(150, 200);
   private final AddressableLEDBufferView leftSection = buffer.createView(200, 300).reversed();
-  private final AddressableLEDBufferView[] sections = { rightSection, topRightSection, topLeftSection, leftSection };
+  private final AddressableLEDBufferView[] sections = {
+    rightSection, topRightSection, topLeftSection, leftSection
+  };
 
   // LED modes
-  public RainbowAnimation rainbowAnim = 
-      new RainbowAnimation(.5, .89, 64);
+  public RainbowAnimation rainbowAnim = new RainbowAnimation(.5, .89, 64);
   public LarsonAnimation larsonAnim =
-      new LarsonAnimation(177, 156, 217); // try again with full string, looks like annoying flashing
+      new LarsonAnimation(
+          177, 156, 217); // try again with full string, looks like annoying flashing
   public TwinkleAnimation twinkleAnim =
       new TwinkleAnimation(135, 30, 270); // cool ig idk test on full string, it twinkle
   public ColorFlowAnimation colorFlowAnim =
@@ -85,13 +86,13 @@ public class ElevatorLight extends SubsystemBase {
 
   public Command animate(LEDPattern animation, String name) {
     return run(() -> {
-        for (AddressableLEDBufferView section : sections) {
-          animation.applyTo(section);
-          for(int i=0; i<section.getLength(); ++i){
-            candle.setLEDs(section.getRed(i), section.getBlue(i), section.getGreen(i), 0, i, 1);
+          for (AddressableLEDBufferView section : sections) {
+            animation.applyTo(section);
+            for (int i = 0; i < section.getLength(); ++i) {
+              candle.setLEDs(section.getRed(i), section.getBlue(i), section.getGreen(i), 0, i, 1);
+            }
           }
-        }
-      }).withName("Animate" + name);
+        })
+        .withName("Animate" + name);
   }
-
-  }
+}
