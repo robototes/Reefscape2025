b4458772523f47f0402f6f3e5312dd045f96f617
RobototesProgrammers <194180343+RobototesProgrammers@users.noreply.github.com>
2025-03-08 14:36:57 -0800

    cam angle fixes?

--- src/main/java/frc/robot/subsystems/VisionSubsystem.java
+++ src/main/java/frc/robot/subsystems/VisionSubsystem.java
@@ -45,8 +45,8 @@ public class VisionSubsystem extends SubsystemBase {
   private static final double CAMERA_Z_POS_METERS_BACK = 0.737;
   private static final double CAMERA_ROLL_FRONT = Units.degreesToRadians(180);
   private static final double CAMERA_ROLL_BACK = 0;
-  private static final double CAMERA_PITCH_FRONT = Units.degreesToRadians(10);
-  private static final double CAMERA_PITCH_BACK = Units.degreesToRadians(10);
+  private static final double CAMERA_PITCH_FRONT = Units.degreesToRadians(-10);
+  private static final double CAMERA_PITCH_BACK = Units.degreesToRadians(-10);
   private static final double CAMERA_YAW_FRONT = Units.degreesToRadians(-28.1);
   private static final double CAMERA_YAW_BACK = Units.degreesToRadians(180);
 
