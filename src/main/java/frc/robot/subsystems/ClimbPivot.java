e8b7be8a76a3ba76f399b98def1cf7abc97eb424
RobototesProgrammers <194180343+RobototesProgrammers@users.noreply.github.com>
2025-03-22 15:45:56 -0700

    Adjust climbed preset

--- src/main/java/frc/robot/subsystems/ClimbPivot.java
+++ src/main/java/frc/robot/subsystems/ClimbPivot.java
@@ -35,7 +35,7 @@ public class ClimbPivot extends SubsystemBase {
 
   private final double STOWED_PRESET = -0.09;
   private final double CLIMB_OUT_PRESET = -0.405;
-  private final double CLIMBED_PRESET = -0.208;
+  private final double CLIMBED_PRESET = -0.120;
   private final double FORWARD_SOFT_STOP = -0.07;
   private final double REVERSE_SOFT_STOP = -78;
   private final double CLIMB_OUT_SPEED = -0.1;
