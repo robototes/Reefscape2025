44eaaefa9a616e6b3d9e32352209c85076b55604
SnappleRamen <130863864+SnappleRamen@users.noreply.github.com>
2025-03-26 19:35:47 -0700

    Imrpoved branch presets for autoalign and added possibly tuned pid values

--- src/main/java/frc/robot/subsystems/auto/AutoAlign.java
+++ src/main/java/frc/robot/subsystems/auto/AutoAlign.java
@@ -64,29 +64,32 @@ public class AutoAlign {
           BRANCH_B_L);
 
   private static final Pose2d BRANCH_R_A =
-      new Pose2d(new Translation2d(14.38, 3.85), Rotation2d.fromDegrees(179.3));
+      new Pose2d(new Translation2d(14.35, 3.85), Rotation2d.fromDegrees(180));
   private static final Pose2d BRANCH_R_B =
-      new Pose2d(new Translation2d(14.36, 4.24), Rotation2d.fromDegrees(180));
+      new Pose2d(new Translation2d(14.354, 4.184), Rotation2d.fromDegrees(180));
   private static final Pose2d BRANCH_R_C =
-      new Pose2d(new Translation2d(13.86, 5.09), Rotation2d.fromDegrees(240));
+      new Pose2d(new Translation2d(13.845, 5.073), Rotation2d.fromDegrees(240));
   private static final Pose2d BRANCH_R_D =
-      new Pose2d(new Translation2d(13.54, 5.24), Rotation2d.fromDegrees(240));
+      new Pose2d(new Translation2d(13.557, 5.252), Rotation2d.fromDegrees(240));
   private static final Pose2d BRANCH_R_E =
-      new Pose2d(new Translation2d(12.54, 5.26), Rotation2d.fromDegrees(-54));
+      new Pose2d(new Translation2d(12.554, 5.230), Rotation2d.fromDegrees(-60));
   private static final Pose2d BRANCH_R_F =
-      new Pose2d(new Translation2d(12.25, 5.03), Rotation2d.fromDegrees(300));
+      new Pose2d(new Translation2d(12.275, 5.065
+      ), Rotation2d.fromDegrees(300));
+        //START FROM HERE AFTERWARDS!  
   private static final Pose2d BRANCH_R_G =
-      new Pose2d(new Translation2d(11.73, 4.18), Rotation2d.fromDegrees(-2.35));
+      new Pose2d(new Translation2d(11.771, 4.19), Rotation2d.fromDegrees(0));
   private static final Pose2d BRANCH_R_H =
-      new Pose2d(new Translation2d(11.72, 3.83), Rotation2d.fromDegrees(-4.9));
+      new Pose2d(new Translation2d(11.769, 3.866), Rotation2d.fromDegrees(0));
+
   private static final Pose2d BRANCH_R_I =
-      new Pose2d(new Translation2d(12.29, 2.95), Rotation2d.fromDegrees(50.4));
+      new Pose2d(new Translation2d(12.29, 2.95), Rotation2d.fromDegrees(60));
   private static final Pose2d BRANCH_R_J =
-      new Pose2d(new Translation2d(12.60, 2.82), Rotation2d.fromDegrees(49.9));
+      new Pose2d(new Translation2d(12.565, 2.82), Rotation2d.fromDegrees(60));
   private static final Pose2d BRANCH_R_K =
-      new Pose2d(new Translation2d(13.59, 2.80), Rotation2d.fromDegrees(120));
+      new Pose2d(new Translation2d(13.558, 2.80), Rotation2d.fromDegrees(120));
   private static final Pose2d BRANCH_R_L =
-      new Pose2d(new Translation2d(13.86, 3.03), Rotation2d.fromDegrees(121.1));
+      new Pose2d(new Translation2d(13.88, 2.996), Rotation2d.fromDegrees(120));
   private static final List<Pose2d> redBranchesPoses =
       Arrays.asList(
           BRANCH_R_A,
@@ -146,7 +149,7 @@ public class AutoAlign {
     }
 
     // sets the point for the path to go to
-    List<Waypoint> waypointsPoeses = PathPlannerPath.waypointsFromPoses(robotPose,branchPose);
+    List<Waypoint> waypointsPoeses = PathPlannerPath.waypointsFromPoses(robotPose, new Pose2d(branchPose.getX(), branchPose.getY(), branchPose.getRotation()));
     // creates path
     PathPlannerPath path =
         new PathPlannerPath(
