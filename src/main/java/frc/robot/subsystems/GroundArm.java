f060ae855f6a06384ff245a0fdd58461c0c38845
Polyehteretherketone <s-parrisr@bsd405.org>
2025-03-27 18:15:18 -0700

    fixed command placeholder

--- src/main/java/frc/robot/subsystems/GroundArm.java
+++ src/main/java/frc/robot/subsystems/GroundArm.java
@@ -6,14 +6,17 @@ import com.ctre.phoenix6.configs.TalonFXConfigurator;
 import com.ctre.phoenix6.controls.MotionMagicVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.NeutralModeValue;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Hardware;
 
 public class GroundArm extends SubsystemBase {
   public static final double STOWED_POSITION = 0.25;
   public static final double GRAB_POSITION = -0.25;
+  public static final double POS_TOLERANCE = Units.degreesToRotations(5);
 
   // MotionMagic voltage
   private final MotionMagicVoltage m_request = new MotionMagicVoltage(0);
@@ -76,16 +79,10 @@ public class GroundArm extends SubsystemBase {
     logTabs();
   }
 
-  private Command armPos(double pos) {
-    return startEnd(
-        () -> {
-          gndmotor.setVoltage(pos);
-          stowedPosition = pos;
-        },
-        () -> gndmotor.stopMotor());
-  }
-
-  public Command stowedPosition() {
-    return armPos(STOWED_POSITION).withName("holdIntakePower");
+  // preset command placeholder
+  public Command moveToPosition(double position) {
+    return setTargetPosition(position)
+        .andThen(
+            Commands.waitUntil(() -> Math.abs(getCurrentPosition() - position) < POS_TOLERANCE));
   }
 }
