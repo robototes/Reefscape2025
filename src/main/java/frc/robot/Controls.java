4effbe6073d5a48808eb5e6317eb61906ab74bde
RobototesProgrammers <194180343+RobototesProgrammers@users.noreply.github.com>
2025-04-04 16:42:19 -0700

    Give spinny claw the right scoring mode

--- src/main/java/frc/robot/Controls.java
+++ src/main/java/frc/robot/Controls.java
@@ -572,7 +572,7 @@ public class Controls {
     if (s.spinnyClawSubsytem == null) {
       return;
     }
-    s.spinnyClawSubsytem.setScoringMode(() -> ScoringMode.CORAL);
+    s.spinnyClawSubsytem.setScoringMode(() -> scoringMode);
     // Claw controls bindings go here
     connected(armPivotSpinnyClawController)
         .and(armPivotSpinnyClawController.leftBumper())
