c90762af9c7e6f61c4b3b6f02bb16c65db196f83
RobototesProgrammers <194180343+RobototesProgrammers@users.noreply.github.com>
2025-04-05 10:03:04 -0700

    Switch to using separate X and angle controllers Also remove unused imports

--- src/main/java/frc/robot/subsystems/auto/BargeAlign.java
+++ src/main/java/frc/robot/subsystems/auto/BargeAlign.java
@@ -2,21 +2,14 @@ package frc.robot.subsystems.auto;
 
 import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
 import com.ctre.phoenix6.swerve.SwerveRequest;
-import edu.wpi.first.apriltag.AprilTagFieldLayout;
-import edu.wpi.first.apriltag.AprilTagFields;
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rectangle2d;
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Transform2d;
-import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.Controls;
 import frc.robot.subsystems.drivebase.CommandSwerveDrivetrain;
-import java.util.List;
 
 public class BargeAlign extends Command {
   private PIDController pidX = new PIDController(4, 0, 0);
@@ -24,10 +17,11 @@ public class BargeAlign extends Command {
   private PIDController pidRotate = new PIDController(8, 0, 0);
 
   private CommandSwerveDrivetrain drive;
-  private Pose2d blueCenterBarge = new Pose2d(7.557, 1.861, Rotation2d.kZero);
-  private Pose2d redCenterBarge = new Pose2d();
-  private Rectangle2d blueBargePose = new Rectangle2d(blueCenterBarge, 1, 3.683);
-  private Rectangle2d redBargePose = new Rectangle2d(redCenterBarge, 1, 3.683);
+  private static final double fieldLength = 17.548; // Welded field
+  private static final double blueBlacklineX = 7.557;
+  private static final double redBlacklineX = fieldLength - blueBlacklineX;
+  private double targetX;
+  private Rotation2d targetAngle;
   private boolean redAlliance;
 
   private final SwerveRequest.FieldCentric driveRequest =
@@ -41,22 +35,25 @@ public class BargeAlign extends Command {
     setName("BargeAlign");
   }
 
+  private boolean atTargetPosition() {
+    Pose2d currentPose = drive.getState().Pose;
+    return Math.abs(targetX - currentPose.getX()) < 0.01
+      && Math.abs(targetAngle.minus(currentPose.getRotation()).getDegrees()) < 1;
+  }
+
   @Override
   public void initialize() {
     redAlliance = DriverStation.getAlliance().get() == Alliance.Red;
-    Pose2d robotPose = drive.getState().Pose;
-    branchPose = getNearestBranch(robotPose, !redAlliance);
-    pidX.setSetpoint(branchPose.getX());
-    pidY.setSetpoint(branchPose.getY());
-    pidRotate.setSetpoint(branchPose.getRotation().getRadians());
+    targetX = redAlliance ? redBlacklineX : blueBlacklineX;
+    targetAngle = redAlliance ? Rotation2d.k180deg : Rotation2d.kZero;
+    pidX.setSetpoint(targetX);
+    pidRotate.setSetpoint(targetAngle.getRadians());
   }
 
   @Override
   public void execute() {
     Pose2d currentPose = drive.getState().Pose;
-    Transform2d robotToBranch = branchPose.minus(currentPose);
-    if (robotToBranch.getTranslation().getNorm() < 0.01
-        && Math.abs(robotToBranch.getRotation().getDegrees()) < 1) {
+    if (atTargetPosition()) {
       return;
     }
     // Calculate the power for X direction and clamp it between -1 and 1
@@ -80,14 +77,7 @@ public class BargeAlign extends Command {
 
   @Override
   public boolean isFinished() {
-    Pose2d currentPose = drive.getState().Pose;
-    Transform2d robotToBranch = branchPose.minus(currentPose);
-    if (robotToBranch.getTranslation().getNorm() < 0.01
-        && Math.abs(robotToBranch.getRotation().getDegrees()) < 1) {
-      return true;
-    } else {
-      return false;
-    }
+    return atTargetPosition();
   }
 
   @Override
