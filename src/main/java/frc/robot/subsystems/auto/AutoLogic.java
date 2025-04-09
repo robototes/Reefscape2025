ec091cf50d34443f6c9216d8a3340a1d668d8b22
Jetblackdragon <64041077+Jetblackdragon@users.noreply.github.com>
2025-04-08 18:49:43 -0700

    Autos post dcmp (#142)
    * More 2 piece autos to be compatiable with good auto alliances
    
    * Ignore one second left auto timeout
    Messed up testing the autos
    
    * Increase path max velocity and acceleration
    
    * Drop intake wait time from 3 seconds to 1.5 seconds
    
    ---------
    
    Co-authored-by: DriverStation3 <194180343+RobototesProgrammers@users.noreply.github.com>

--- src/main/java/frc/robot/subsystems/auto/AutoLogic.java
+++ src/main/java/frc/robot/subsystems/auto/AutoLogic.java
@@ -107,7 +107,13 @@ public class AutoLogic {
           new AutoPath("OSMRSF_D-C", "OSMRSF_D-C"),
           new AutoPath("OSWRSF_F-E", "OSWRSF_F-E"),
           new AutoPath("OSWRSF_E-D", "OSWRSF_E-D"),
-          new AutoPath("OSWRSF_D-C", "OSWRSF_D-C"));
+          new AutoPath("OSWRSF_D-C", "OSWRSF_D-C"),
+          new AutoPath("MLSF_H-I", "MLSF_H-I"),
+          new AutoPath("MLSF_H-I_WithWait", "MLSF_H-I_WithWait"),
+          new AutoPath("MRSF_G-F", "MRSF_G-F"),
+          new AutoPath("MRSF_G-F_WithWait", "MRSF_G-F_WithWait"),
+          new AutoPath("MRSF_G-H", "MRSF_G-H"),
+          new AutoPath("MLSF_H-G", "MLSF_H-G"));
 
   private static List<AutoPath> threePiecePaths =
       List.of(
@@ -288,7 +294,7 @@ public class AutoLogic {
   public static Command isCollected() {
     if (ARMSENSOR_ENABLED && r.sensors.armSensor != null) {
       return Commands.waitUntil(r.sensors.armSensor.inTrough())
-          .withTimeout(3)
+          .withTimeout(1.5)
           .withName("isCollected");
     }
     return Commands.none().withName("isCollected");
