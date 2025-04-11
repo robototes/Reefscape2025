b760d7c23c13620c367874b8a8282437b347a002
RobototesProgrammers <194180343+RobototesProgrammers@users.noreply.github.com>
2025-04-11 16:45:57 -0700

    added match tab

---
+++ src/main/java/frc/robot/util/MatchTab.java
@@ -0,0 +1,45 @@
+package frc.robot.util;
+
+import static edu.wpi.first.units.Units.Meters;
+
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import frc.robot.Sensors;
+import frc.robot.Subsystems;
+
+public class MatchTab {
+  public static void create(Sensors sensors, Subsystems subsystems) {
+    var tab = Shuffleboard.getTab("Match");
+    tab.addBoolean(
+            "has arm sensor",
+            () ->
+                (sensors.armSensor != null && sensors.armSensor.getSensorDistance().in(Meters) > 0))
+        .withSize(1, 1)
+        .withPosition(0, 0)
+        .withWidget(BuiltInWidgets.kBooleanBox);
+    tab.addBoolean(
+            "has ground intake sensor",
+            () ->
+                (sensors.intakeSensor != null
+                    && sensors.intakeSensor.getSensorDistance().in(Meters) > 0))
+        .withSize(1, 1)
+        .withPosition(1, 0)
+        .withWidget(BuiltInWidgets.kBooleanBox);
+    tab.addBoolean(
+            "has left branch sensor",
+            () ->
+                (sensors.branchSensors != null
+                    && sensors.branchSensors.getLeftSensorDistance().in(Meters) > 0))
+        .withSize(1, 1)
+        .withPosition(0, 1)
+        .withWidget(BuiltInWidgets.kBooleanBox);
+    tab.addBoolean(
+            "has right branch sensor",
+            () ->
+                (sensors.branchSensors != null
+                    && sensors.branchSensors.getRightSensorDistance().in(Meters) > 0))
+        .withSize(1, 1)
+        .withPosition(1, 1)
+        .withWidget(BuiltInWidgets.kBooleanBox);
+  }
+}
