820357e84041cc5b7320516e0f92b7325baf4e9e
Jetblackdragon <64041077+Jetblackdragon@users.noreply.github.com>
2025-10-20 15:13:16 -0700

    current limits for motors

--- src/main/java/frc/robot/generated/CompTunerConstants.java
+++ src/main/java/frc/robot/generated/CompTunerConstants.java
@@ -80,7 +80,12 @@ public class CompTunerConstants {
 
   // Initial configs for the drive and steer motors and the azimuth encoder; these cannot be null.
   // Some configs will be overwritten; check the `with*InitialConfigs()` API documentation.
-  private static final TalonFXConfiguration driveInitialConfigs = new TalonFXConfiguration();
+  private static final TalonFXConfiguration driveInitialConfigs = new TalonFXConfiguration().withCurrentLimits(
+    new CurrentLimitsConfigs()
+        .withStatorCurrentLimit(Amps.of(160))
+        .withStatorCurrentLimitEnable(true)
+        .withSupplyCurrentLimit(Amps.of(100))
+        .withSupplyCurrentLimitEnable(true));;
   private static final TalonFXConfiguration steerInitialConfigs =
       new TalonFXConfiguration()
           .withCurrentLimits(
@@ -89,7 +94,9 @@ public class CompTunerConstants {
                   // low
                   // stator current limit to help avoid brownouts without impacting performance.
                   .withStatorCurrentLimit(Amps.of(60))
-                  .withStatorCurrentLimitEnable(true));
+                  .withStatorCurrentLimitEnable(true)
+                  .withSupplyCurrentLimit(Amps.of(40))
+                  .withSupplyCurrentLimitEnable(true));
   private static final CANcoderConfiguration encoderInitialConfigs = new CANcoderConfiguration();
   // Configs for the Pigeon 2; leave this null to skip applying Pigeon 2 configs
   private static final Pigeon2Configuration pigeonConfigs = null;
