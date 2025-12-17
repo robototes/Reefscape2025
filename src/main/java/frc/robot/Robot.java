1e042b955e1c5717071a56d7c11cd20327c70638
SnappleRamen <130863864+SnappleRamen@users.noreply.github.com>
2025-12-16 22:46:35 -0800

    Still some jitter but getting there maybe

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -6,6 +6,10 @@ package frc.robot;
 
 import au.grapplerobotics.CanBridge;
 import com.pathplanner.lib.commands.FollowPathCommand;
+import com.therekrab.autopilot.APTarget;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.net.WebServer;
 import edu.wpi.first.wpilibj.DataLogManager;
 import edu.wpi.first.wpilibj.DriverStation;
@@ -18,11 +22,15 @@ import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.Subsystems.SubsystemConstants;
 import frc.robot.subsystems.SuperStructure;
+import frc.robot.subsystems.auto.AutoAlign;
 import frc.robot.subsystems.auto.AutoBuilderConfig;
 import frc.robot.subsystems.auto.AutoLogic;
+import frc.robot.subsystems.auto.AutoPilotFinder;
 import frc.robot.subsystems.auto.AutonomousField;
+import frc.robot.subsystems.auto.AutoAlign.AlignType;
 import frc.robot.util.BuildInfo;
 import frc.robot.util.MatchTab;
 import frc.robot.util.RobotType;
@@ -145,7 +153,12 @@ public class Robot extends TimedRobot {
   public void autonomousInit() {
     Shuffleboard.startRecording();
     if (SubsystemConstants.DRIVEBASE_ENABLED && AutoLogic.getSelectedAuto() != null) {
-      AutoLogic.getSelectedAuto().schedule();
+
+      //AutoLogic.getSelectedAuto().schedule();
+      subsystems.drivebaseSubsystem.resetPose(subsystems.drivebaseSubsystem.getState().Pose);
+      Commands.sequence(
+      AutoPilotFinder.createAutopilotCommand(new APTarget(new Pose2d(10,5,subsystems.drivebaseSubsystem.getState().Pose.getRotation())), subsystems.drivebaseSubsystem),
+      AutoAlign.autoAlign(subsystems.drivebaseSubsystem, controls, AlignType.ALLB)).schedule();
     }
     if (subsystems.climbPivotSubsystem != null) {
       subsystems.climbPivotSubsystem.moveCompleteFalse();
---
+++ src/main/java/frc/robot/subsystems/auto/AutoPilotFinder.java
@@ -0,0 +1,102 @@
+package frc.robot.subsystems.auto;
+
+import java.io.IOException;
+import org.json.simple.parser.ParseException;
+import com.ctre.phoenix6.swerve.SwerveRequest;
+import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
+import com.ctre.phoenix6.swerve.SwerveRequest.ForwardPerspectiveValue;
+import com.pathplanner.lib.config.RobotConfig;
+import com.therekrab.autopilot.APConstraints;
+import com.therekrab.autopilot.APProfile;
+import com.therekrab.autopilot.APTarget;
+import com.therekrab.autopilot.Autopilot;
+import com.therekrab.autopilot.Autopilot.APResult;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.units.Units;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.generated.CompTunerConstants;
+import frc.robot.subsystems.drivebase.CommandSwerveDrivetrain;
+
+public class AutoPilotFinder extends SubsystemBase {
+
+
+  //Huge thanks to 1458(Red Tie Robotics) for providing example code
+
+
+
+  private static APConstraints constraints;
+
+static {
+    try {
+        var config = RobotConfig.fromGUISettings();
+        constraints = new APConstraints()
+            .withVelocity(config.moduleConfig.maxDriveVelocityMPS)
+            .withAcceleration(config.moduleConfig.driveCurrentLimit)
+            .withJerk(3.0);
+    } catch (IOException | ParseException e) {
+        e.printStackTrace();  // log the error
+        // fallback values if GUI config fails
+        constraints = new APConstraints()
+            .withVelocity(6)   // m/s
+            .withAcceleration(4) // m/s²
+            .withJerk(3.0);
+    }
+}
+
+		
+  private static double kP = CompTunerConstants.FrontRight.SteerMotorGains.kP;
+  private static double kI = CompTunerConstants.FrontRight.SteerMotorGains.kI;
+  private static double kD = CompTunerConstants.FrontRight.SteerMotorGains.kD;
+  private static final APProfile profile = new APProfile(constraints)
+    .withErrorXY(Units.Centimeters.of(5))
+    .withErrorTheta(Units.Degrees.of(0.5))
+    .withBeelineRadius(Units.Centimeters.of(3));
+ 
+  public static final Autopilot autoPilot = new Autopilot(profile); // autopilot insance with the desired profile based on the constraints from pathplanner maybe
+ 
+
+ 
+ 
+ 
+ public static Command createAutopilotCommand(APTarget target, CommandSwerveDrivetrain drive) {
+     return drive.run(() -> {
+         // Current robot speeds & pose
+         ChassisSpeeds robotRelativeSpeeds =   // Converted from field relative to robot relative 
+     ChassisSpeeds.fromFieldRelativeSpeeds(
+         drive.getState().Speeds.vxMetersPerSecond,
+         drive.getState().Speeds.vyMetersPerSecond,
+         drive.getState().Speeds.omegaRadiansPerSecond,
+         drive.getState().Pose.getRotation()
+     );
+         Pose2d pose = drive.getState().Pose;
+ 
+         // Autopilot calculation
+         APResult out = autoPilot.calculate(pose, robotRelativeSpeeds, target);  // Handles calculations for going to the set point
+ 
+         // SwerveRequest to drive the robot to the designated point
+         drive.setControl(
+             new SwerveRequest.FieldCentricFacingAngle()
+                 .withForwardPerspective(ForwardPerspectiveValue.BlueAlliance)
+                 .withDriveRequestType(DriveRequestType.Velocity)
+                 .withHeadingPID(kP,kI,kD
+                    )
+                .withVelocityX(out.vx())
+                .withVelocityY(out.vy())
+                .withTargetDirection(out.targetAngle())
+        );
+    }).withName("AutopilotCommand").until(() -> autoPilot.atTarget(drive.getState().Pose, target));
+
+
+
+
+    
+}
+
+
+}
+  
+
+
+
